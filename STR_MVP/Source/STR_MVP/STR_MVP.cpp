54e2957611096105991ec6b0d183ee6ec1b50ce8
MrCelestio <ebb618@gmail.com>
2019-09-29 22:27:33 -0400

    Initial Push

---
+++ STR_MVP/Source/STR_MVP/STR_MVP.cpp
@@ -0,0 +1,9 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "STR_MVP.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, STR_MVP, "STR_MVP" );
+
+DEFINE_LOG_CATEGORY(LogSTR_MVP)
+ 
---
+++ STR_MVP/Source/STR_MVP/STR_Main_GameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "STR_Main_GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STR_MVP_API ASTR_Main_GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
